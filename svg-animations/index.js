5b1a785abceac31a997b122ef3dd91377474b048
DBarnes0451 <48840911+DBarnes0451@users.noreply.github.com>
2020-04-01 03:48:18 +0100

    Add files via upload

---
+++ svg-animations/index.js
@@ -0,0 +1,47 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('heart-anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'heart-animation.json'
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('star-anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'star-animation.json'
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('cog-anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'cog-animation.json'
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('mail-anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'mail-animation.json'
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('call-anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'call-animation.json'
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('share-anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'share-animation.json'
+  })
